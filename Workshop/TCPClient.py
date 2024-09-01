f838fab0012d3df5413b501f62ff4758702d8dee
ahmed samir <modysamir459@gmail.com>
2024-09-01 17:20:51 +0300

    .

--- Workshop/TCPClient.py
+++ Workshop/TCPClient.py
@@ -1,14 +1,14 @@
 import socket as sk 
 serverIP= input("Enter Server IP address: ")
 serverPort= int(input("Enter Server Port: "))
-clientSocket= sk.socket(sk.AF_INET, sk.SOCK_STREAM)#open socket for TCP "SOCK_STREAM" connection and IPv4 "AF_INET" address family
+clientSocket= sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-clientSocket.connect((serverIP,serverPort))# connect to server with IP and Port 
+clientSocket.connect((serverIP,serverPort))
 while True:
     data= input("Enter Data or 0 to exit: ")
     if data =='0':
         break
-    clientSocket.sendall(data.encode()) # send data to server  
-    receivedData=clientSocket.recv(1024) # receive data from server
+    clientSocket.sendall(data.encode())  
+    receivedData=clientSocket.recv(1024) 
     print(f"Data received from server is \"{receivedData.decode()}\"")
-clientSocket.close() # close connection with server
+clientSocket.close() 
--- Workshop/TCPServer.py
+++ Workshop/TCPServer.py
@@ -1,41 +1,54 @@
 import socket as sk
 import threading
 
-closeThreads=False
+closeThreads = False
 numberOfConnections = 0
-connectionThreads= []
-server_ip=sk.gethostbyname(sk.gethostname()) #set server ip to the ip of the machine 
-server_port= 12345
+connectionThreads = []
+server_ip = sk.gethostbyname(sk.gethostname())
+server_port = 12345
 
-def manageClient(clientSocket:sk.socket, addr): #function to manage client connection by receiving and sending data
+def manageClient(clientSocket: sk.socket, addr):
     global numberOfConnections
     while not closeThreads:
         data = clientSocket.recv(1024)
         if not data:
             break
-        data=data.decode()
-        print(f'[{addr[0]}:{addr[1]}]:{data}')
-        message= f'I received from you this message : \"{data}\"'
-        clientSocket.sendall(message.encode('UTF-8'))
+        
+        data = data.decode()
+        print(f'[{addr[0]}:{addr[1]}]: {data}')
+        
+        if data.startswith("GET") or data.startswith("POST"):  # Handle HTTP requests from a browser
+            response = """\
+HTTP/1.1 200 OK
+
+<html>
+<head><title>Server Response</title></head>
+<body><h1>Received your message!</h1></body>
+</html>
+"""
+            clientSocket.sendall(response.encode('UTF-8'))
+        else:  # Handle other device connections
+            message = f'I received from you this message: \"{data}\"'
+            clientSocket.sendall(message.encode('UTF-8'))
+    
     print(f'Closing Connection to client: {addr[0]}:{addr[1]}')
     clientSocket.close()
-    numberOfConnections-=1
+    numberOfConnections -= 1
 
+server_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+server_socket.bind((server_ip, server_port))
 
-server_socket= sk.socket(sk.AF_INET, sk.SOCK_STREAM) #open socket for TCP "SOCK_STREAM" connection and IPv4 "AF_INET" address family
-server_socket.bind((server_ip,server_port)) #bind socket to server ip and port
-
-server_socket.listen(5) #listen for connections with maximum of 5 connections in queue
-print(f'Listening on IP: {server_port}  and Port: {server_port}...') 
+server_socket.listen(5)
+print(f'Listening on IP: {server_ip} and Port: {server_port}...')
 try:
     while True:
         print(f'Number of connections= {numberOfConnections}')
-        client_sock, client_addr= server_socket.accept() #accept connection from client and return socket and address
+        client_sock, client_addr = server_socket.accept()
         print(f"Connected to {client_addr}")
-        thread= threading.Thread(target=manageClient, args=(client_sock,client_addr,)) #create thread to manage client connection
-        numberOfConnections+=1
-        thread.start() #start thread 
-        connectionThreads.append(thread) 
+        thread = threading.Thread(target=manageClient, args=(client_sock, client_addr,))
+        numberOfConnections += 1
+        thread.start()
+        connectionThreads.append(thread)
 except:
-    server_socket.close() #close server socket
-    closeThreads= True
+    server_socket.close()
+    closeThreads = True
